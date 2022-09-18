3821591a0a5a60534c4b562af5e29428a883326a
LucasMelvin15 <melvinlucas15@gmail.com>
2022-09-18 07:40:11 +0300

    added a post form and rendering to comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,79 +1,78 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
-const Comment = require("../models/Comment")
 
 module.exports = {
-  getProfile: async (req, res) => {
-    try {
-      const posts = await Post.find({ user: req.user.id }); //create a posts array that goes into the Posts collection to find users with the id
-      res.render("profile.ejs", { posts: posts, user: req.user }); //rendering the posts and user
-      // profile.ejs renders the username and email and a form that handles users new posts
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  getFeed: async (req, res) => {
-    try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean(); // go to the Post collection and sort in descending order so its a query to return all the post collection .lean()- returns a javascript object instead of a mongo document
-      res.render("feed.ejs", { posts: posts });// rendering the feedjs after getting all the posts that loops thru the posts by id and images.
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  getPost: async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id); // goes to the post collection to find by id 
-      const comments = await Comment.find({post:req.params.id}).sort({ createdAt: "desc" }).lean()
-      res.render("post.ejs", { post: post, user: req.user, comments: comments }); //
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  createPost: async (req, res) => {
-    try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
+	getProfile: async (req, res) => {
+		try {
+			const posts = await Post.find({ user: req.user.id });
+			res.render("profile.ejs", { posts: posts, user: req.user });
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	getFeed: async (req, res) => {
+		try {
+			const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+			res.render("feed.ejs", { posts: posts });
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	getPost: async (req, res) => {
+		try {
+			const post = await Post.findById(req.params.id)
+				//get the comment information from the database
+				.populate('comments');
+			res.render("post.ejs", { post: post, user: req.user });
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	createPost: async (req, res) => {
+		try {
+			// Upload image to cloudinary
+			const result = await cloudinary.uploader.upload(req.file.path);
 
-      await Post.create({
-        title: req.body.title,
-        image: result.secure_url,
-        cloudinaryId: result.public_id,
-        caption: req.body.caption,
-        likes: 0,
-        user: req.user.id,
-      });
-      console.log("Post has been added!");
-      res.redirect("/profile");
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  likePost: async (req, res) => {
-    try {
-      await Post.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-          $inc: { likes: 1 },
-        }
-      );
-      console.log("Likes +1");
-      res.redirect(`/post/${req.params.id}`);
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  deletePost: async (req, res) => {
-    try {
-      // Find post by id
-      let post = await Post.findById({ _id: req.params.id });
-      // Delete image from cloudinary
-      await cloudinary.uploader.destroy(post.cloudinaryId);
-      // Delete post from db
-      await Post.remove({ _id: req.params.id });
-      console.log("Deleted Post");
-      res.redirect("/profile");
-    } catch (err) {
-      res.redirect("/profile");
-    }
-  },
+			await Post.create({
+				title: req.body.title,
+				image: result.secure_url,
+				cloudinaryId: result.public_id,
+				caption: req.body.caption,
+				likes: 0,
+				user: req.user.id,
+			});
+			console.log("Post has been added!");
+			res.redirect("/profile");
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	likePost: async (req, res) => {
+		try {
+			await Post.findOneAndUpdate(
+				{ _id: req.params.id },
+				{
+					$inc: { likes: 1 },
+				}
+			);
+			console.log("Likes +1");
+			res.redirect(`/post/${req.params.id}`);
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	deletePost: async (req, res) => {
+		try {
+			// Find post by id
+			let post = await Post.findById({ _id: req.params.id });
+			// Delete image from cloudinary
+			await cloudinary.uploader.destroy(post.cloudinaryId);
+			// Delete post from db
+			await Post.remove({ _id: req.params.id });
+			console.log("Deleted Post");
+			res.redirect("/profile");
+		} catch (err) {
+			res.redirect("/profile");
+		}
+	},
 };
